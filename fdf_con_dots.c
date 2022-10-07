0a9fad93e6f3201c66a5fe21a032f2f528568541
Daejin Lee <daejlee@c9r3s9.42seoul.kr>
2022-10-07 13:54:39 +0900

    mandatory draft completed.

--- fdf_con_dots.c
+++ fdf_con_dots.c
@@ -8,82 +8,49 @@
 . . .
 . . .		아니 우단이랑 하단만 처리하면 되자나..
 */
-/*
-void	no_x_offset(fdf_t_info *p, int x, int from_y, int to_y)
-{
-	int	i;
-	int	y_offset;
-
-	i = 0;
-	y_offset = to_y - from_y;
-	while (from_y != to_y)
-	{
-		get_grad_color(x, )
-		mlx_pixel_put(p->mp, p->wp1, x, from_y, )
-	}
-}
-
-void	x_crit_offset(fdf_t_info *p, int grad, double x_offset, int from_color, int to_color, int *from, int *to)
-{
-	int				color;
-	int				i;
-	double			grad_color;
-
-	i = 0;
-	grad_color = (to_color - from_color) / x_offset;
-	while (i != x_offset)
-	{
-		color = grad_color * i - grad_color * i + from_color;
-		mlx_pixel_put(p->mp, p->wp1,
-		from[0] + i,
-		grad * (from[0] + i) - grad * to[0] + to[1],
-		color);
-		if (x_offset < 0)
-			i--;
-		else
-			i++;
-	}
-}
-
-void	y_crit_offset(fdf_t_info *p, int grad, double y_offset, int from_color, int to_color, int *from, int *to)
-{
-	int				color;
-	int				i;
-	double			grad_color;
-
-	i = 0;
-	grad_color = (to_color - from_color) / y_offset;
-	while (i != y_offset)
-	{
-		color = grad_color * i - grad_color * i + from_color;
-		mlx_pixel_put(p->mp, p->wp1,
-		grad * (from[1] + i) - grad * to[1] + to[0],
-		from[1] + i,
-		color);
-		if (y_offset < 0)
-			i--;
-		else
-			i++;
-	}
-}
 
 void	dot_link(fdf_t_info *p, int *from, int *to, int from_color, int to_color)
 {
 	double			x_offset;
 	double			y_offset;
+	double			crit_offset;
+	int				color;
+	int				i;
+	double			grad_color;
+	double			grad;
 
 	x_offset = to[0] - from[0];
 	y_offset = to[1] - from[1];
 	if (!x_offset || !y_offset)
 		return ;
-	//if (fabs(x_offset) > fabs(y_offset))
-		x_crit_offset(p, y_offset / x_offset, x_offset, from_color, to_color, from, to);
-	//else
-		//y_crit_offset(p, x_offset / y_offset, y_offset, from_color, to_color, from, to);
+	i = 0;
+	grad = y_offset / x_offset;
+	if (fabs(grad) > 1)
+	{
+		crit_offset = y_offset;
+		grad_color = (to_color - from_color) / y_offset;
+		grad = 1 / grad;
+	}
+	else
+	{
+		crit_offset = x_offset;
+		grad_color = (to_color - from_color) / x_offset;
+	}
+	while (i != crit_offset)
+	{
+		color = grad_color * i - grad_color * i + from_color;
+		if (fabs(y_offset) > fabs(x_offset))
+			mlx_pixel_put(p->mp, p->wp1, grad * (from[1] + i) - grad * to[1] + to[0], from[1] + i, color);
+		else
+			mlx_pixel_put(p->mp, p->wp1, from[0] + i, grad * (from[0] + i) - grad * to[0] + to[1], color);
+		if (crit_offset < 0)
+			i--;
+		else
+			i++;
+	}
 }
-*/
 
-void	dot_link(fdf_t_info *p, int *from, int *to, int from_color, int to_color)
+void	dot_link_ori(fdf_t_info *p, int *from, int *to, int from_color, int to_color)
 {
 	double			x_offset;
 	double			y_offset;
